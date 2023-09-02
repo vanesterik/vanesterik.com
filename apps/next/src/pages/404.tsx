edcb236051e06cb5df43b96cb8a4aa9f11cc96ea
Koen Dirk van Esterik <koen@vanesterik.com>
2023-09-02 22:19:07 +0200

    feat: add not found page

---
+++ apps/next/src/pages/404.tsx
@@ -0,0 +1,7 @@
+import { ComingSoon } from '../components'
+
+export default function NotFound() {
+  return (
+    <h1 className="font-bold text-black text-8xl dark:text-white">not found</h1>
+  )
+}
