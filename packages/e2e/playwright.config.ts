ff11a31f23f81adf91c1e052ef3e7e4a29f244d1
Koen van Esterik <koen@vanesterik.com>
2025-08-12 15:30:40 +0200

    test: implement playright report summary #3

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -22,7 +22,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    [
+      'html',
+      {
+        open: process.env.CI ? 'never' : 'on-failure',
+      },
+    ],
+    ['list'],
+    ['json', { outputFile: 'test-results/results.json' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
